eeea945deab4c11b686cd7529fd7196cf1999fd6
Robson Vaamonde <robsonvaamonde@gmail.com>
2020-10-25 14:11:18 -0300

    Atualizacao

--- startvm.sh
+++ startvm.sh
@@ -8,8 +8,8 @@
 # Instagram Procedimentos em TI: https://www.instagram.com/procedimentoem/
 # YouTUBE Bora Para Prática: https://www.youtube.com/boraparapratica
 # Data de criação: 21/09/2019
-# Data de atualização: 23/09/2019
-# Versão: 0.04
+# Data de atualização: 15/10/2020
+# Versão: 0.05
 # Testado e homologado para a versão do Linux Mint 19.x x64
 # Kernel >= 4.15.x e 5.0.x
 
@@ -28,11 +28,11 @@
 # Vídeo aula de Básico de Git e Github: https://www.youtube.com/watch?v=MkKkZA8wxHU&t
 
 # Declarando as variáveis de ambiente do script
-VM="Win7-RDesktop"
+VM="Windows"
 USERNAME="vaamonde"
 DOMAIN="WORKGROUP"
-PASSWORD="pti@2019"
-HOST="192.168.1.150"
+PASSWORD="123456"
+HOST="192.168.0.174"
 RDP_PORT="3389"
 KEY="pt-br"
 COLOR="16"
@@ -41,10 +41,10 @@ RESOLUTION="1024x768"
 # Em desenvolvimento (falhas de compilação, acesso remoto e execução de aplicativos seamless)
 # Acompanhe o arquivo CHANGELOG sobre as mudanças do script e correções: https://github.com/vaamonde/virtualbox/blob/master/CHANGELOG
 # Reporte falhas, erros, melhorias, sugestões na opção: ISSUE do Github: https://github.com/vaamonde/virtualbox/issues
-# SEAMLESS="c:\seamlessrdp\seamlessrdpshell.exe"
+SEAMLESS="c:\seamlessrdp\seamlessrdpshell.exe"
 # API="excel.exe"
 
-# Documentação dos comandos utilizandos nesse script:
+# Documentação dos comandos utilizados nesse script:
 # case = https://ss64.com/bash/case.html
 # echo = https://ss64.com/bash/echo.html
 # sleep = https://ss64.com/bash/sleep.html
@@ -65,7 +65,7 @@ case "$1" in
         echo "Inicializando a Máquina Virtual $VM"
         nohup VBoxHeadless --startvm $VM &2> /dev/null &
         sleep 2
-        echo "Máquina inicializada, aguarde para testar o acesso remoto a $VM"
+        echo "Máquina inicializada, aguarde para testar o acesso remoto na máquina $VM"
         echo "Selecione test ou remote para testar o acesso remoto a $VM"
         ;;
 
@@ -78,7 +78,7 @@ case "$1" in
         ;;
 
   save)
-        echo "Salvando a Máquina Virtual $VM"
+        echo "Salvando o Status da Máquina Virtual $VM"
         nohup VBoxManage controlvm $VM savestate &2> /dev/null &
         sleep 2
         echo "Máquina salva, selecione start para iniciar a Máquina Virtual $VM"
@@ -127,7 +127,7 @@ case "$1" in
         echo
         echo "Pingando a Máquina Virtual $VM com o Endereço IP $HOST, aguarde..."
         ping -c 4 $HOST
-        echo "Teste de ping realizado, continuando com teste..."
+        echo "Teste de ping realizado com sucesso, continuando com teste..."
         sleep 3
         echo
         echo "Verificando a Porta de Conexão do RDP da Máquina Virtual $VM com o Endereço IP $HOST, aguarde..."
@@ -177,10 +177,22 @@ case "$1" in
         echo "Acesso remoto feito com sucesso na Máquina Virtual $VM"
         ;;
 
+  seamlesson)
+		echo "Habilitando o Recurso do Seamless na Máquina Virtual $VM"
+		VBoxManage setextradata $VM GUI/Seamless on
+		echo "Seamleass habilitado com sucesso na Máquina Virtual $VM"
+		;;
+
+  seamlessoff)
+		echo "Desabilitando o Recurso do Seamless na Máquina Virtual $VM"
+		VBoxManage setextradata $VM GUI/Seamless off
+		echo "Seamleass desabilitado com sucesso na Máquina Virtual $VM"
+		;;
+
 # Em desenvolvimento (falhas de compilação, acesso remoto e execução de aplicativos seamless)
 #  notepad)
 #        echo "Acessando Remotamente ao Bloco de Notas Seamless na Máquina Virtual $VM"
-#        rdesktop-vrdp -u $USERNAME -d $DOMAIN -p $PASSWORD -A $SEAMLESS -s 'notepad.exe' -k $KEY -b -a $COLOR $HOST &2> /dev/null &
+#        rdesktop-vrdp -u $USERNAME -d $DOMAIN -p $PASSWORD -A "c:\seamlessrdp\seamlessrdpshell.exe notepad" -k $KEY -b -a $COLOR $HOST &2> /dev/null &
 #        sleep 2
 #        echo "Acesso remoto feito com sucesso na Máquina Virtual $VM"
 #        ;;
@@ -194,22 +206,24 @@ case "$1" in
 
   *)
         echo        
-        echo "Escolha as opções: sh $0 {start|pause|save|reset|restart|stop|test|list|run|info|remote|full}" >&2
+        echo "Escolha as opções: sh $0 {start|pause|save|reset|restart|stop|test|list|run|info|remote|full|seamlesson|seamlessoff}" >&2
         echo
-        echo "sh startvm.sh start   - Inicia a máquina virtual $VM"
-        echo "sh startvm.sh pause   - Pausa a máquina virtual $VM"
-        echo "sh startvm.sh save    - Salva a máquina virtual $VM"
-        echo "sh startvm.sh reset   - Reinicia a máquina virtual $VM"
-        echo "sh startvm.sh restart - Para e Inicia a máquina virtual $VM"
-        echo "sh startvm.sh stop    - Para a máquina virtual $VM"
+        echo "sh startvm.sh start		- Inicia a máquina virtual $VM"
+        echo "sh startvm.sh pause		- Pausa a máquina virtual $VM"
+        echo "sh startvm.sh save		- Salva a máquina virtual $VM"
+        echo "sh startvm.sh reset		- Reinicia a máquina virtual $VM"
+        echo "sh startvm.sh restart		- Para e Inicia a máquina virtual $VM"
+        echo "sh startvm.sh stop		- Para a máquina virtual $VM"
         #Em desenvolvimento (falha para desligar o Windows 7 por ACPI Advanced Configuration and Power Interface)
         #echo "sh startvm.sh poweroff - Desliga a máquina virtual $VM"
-        echo "sh startvm.sh test    - Testa o acesso a máquina virtual $VM"
-        echo "sh startvm.sh list    - Lista todas as máquinas virtuais criadas no VirtualBOX"
-        echo "sh startvm.sh run     - Lista todas as máquinas virtuais iniciadas no VirtualBOX"
-        echo "sh startvm.sh info    - Informações detalhadas da máquina virtual $VM"
-        echo "sh startvm.sh remote  - Acesso remoto a máquina virtual $RESOLUTION $VM"
-        echo "sh startvm.sh full    - Acesso remoto a máquina virtual full screen $VM"       
+        echo "sh startvm.sh test		- Testa o acesso a máquina virtual $VM"
+        echo "sh startvm.sh list		- Lista todas as máquinas virtuais criadas no VirtualBOX"
+        echo "sh startvm.sh run		- Lista todas as máquinas virtuais iniciadas no VirtualBOX"
+        echo "sh startvm.sh info		- Informações detalhadas da máquina virtual $VM"
+        echo "sh startvm.sh remote		- Acesso remoto a máquina virtual $RESOLUTION $VM"
+        echo "sh startvm.sh full		- Acesso remoto a máquina virtual full screen $VM"
+	  echo "sh startvm.sh seamlesson	- Acesso remoto a máquina virtual full screen $VM"  
+	  echo "sh startvm.sh seamlessoff	- Acesso remoto a máquina virtual full screen $VM"  
         #Em desenvolvimento (falhas de compilação, acesso remoto e execução de aplicativos seamless)
         #echo "sh startvm.sh notepad - Acesso remoto ao software Notepad Seamless na máquina virtual $VM"
         #echo "sh startvm.sh excel   - Acesso remoto ao software Excel Seamless na máquina virtual $VM"
